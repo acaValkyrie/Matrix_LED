1219d73e99f0171401b4d66c9c0e41db5d2e6c75
Yadokame <yadokame3.14@gmail.com>
2023-05-11 19:13:37 +0900

    led light!

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <Arduino.h>
+#include <ShiftRegister.h>
 
-// put function declarations here:
-int myFunction(int, int);
+ShiftRegister shift_register = ShiftRegister(2, 3, 4);
+
+class Matrix{
+  private:
+    static const int vertical_count   = 8;
+    static const int horizontal_count = 8;
+    byte matrix[vertical_count*horizontal_count] = {0};
+  
+  public:
+    byte GetRow(int index /* 0 <= index <= vertical_count*/){
+      byte row = 0;
+      for(int i = 0; i < horizontal_count; i++){
+        int matrix_index = i + index*horizontal_count;
+        row = row << 1;
+        row = row + matrix[matrix_index];
+      }
+      return row;
+    }
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  shift_register.setup();
+  byte vertical   = B01111111;
+  byte horizontal = B01000000;
+  shift_register.setLedRow(vertical);
+  shift_register.setLedRow(horizontal);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
